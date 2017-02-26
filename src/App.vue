75f0c5991fa734d3ebf2e18750f07896eb7c8c05
Eun Jung Choi <im.your.energy@gmail.com>
2017-02-26 20:42:38 +0900

    ADD pan image

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app">
-    <img src="https://vuejs.org/images/logo.png" alt="Vue logo">
+    <div id="app">
+        <img src="https://vuejs.org/images/logo.png" alt="Vue logo">
 
-    <div>
-      <ul>
-          <li v-for="image in images"><a @click.prevent="showModal(image)" :href="image">{{image}}</a></li>
-      </ul>
+        <div>
+            <ul>
+                <li v-for="image in images"><a @click.prevent="showModal(image)" :href="image">{{image}}</a></li>
+            </ul>
+        </div>
+        <image-modal v-if="selectedImageSrc" :image="selectedImageSrc" @close="closeModal"></image-modal>
     </div>
-    <image-modal v-if="selectedImageSrc" :image="selectedImageSrc" @close="closeModal"></image-modal>
-  </div>
 </template>
 
 <script>
@@ -18,8 +18,6 @@ import ImageModal from './ImageModal.vue';
 export default {
     data: function() {
         return {
-            greeting: 'Welcome to your Vue.js app!',
-            docsURL: 'http://vuejs.org/guide/',
             images: [
                 require('./assets/small1.jpg'),
                 require('./assets/wide1.jpg'),
@@ -33,11 +31,6 @@ export default {
         ImageModal: ImageModal,
     },
     methods: {
-        humanizeURL: function (url) {
-            return url
-                .replace(/^https?:\/\//, '')
-                .replace(/\/$/, '')
-        },
         showModal: function(image) {
             this.selectedImageSrc = image;
         },
--- src/ImageModal.vue
+++ src/ImageModal.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="modal-main" v-show="image">
-      <header class="modal-header">
-        <button class="btn-close" @click="close">x</button>
-      </header>
-      <div class="container">
-        <img :style="zoomStyle" :src="image">
-      </div>
-      <footer class="modal-footer">
-        <button class="zoom-in btn-zoom" @click="zoomIn">+</button>
-        <button class="zoom-reset btn-zoom" @click="zoomReset">=</button>
-        <button class="zoom-out btn-zoom" @click="zoomOut">-</button>
-      </footer>
+        <header>
+            <button class="btn-close" @click="close">x</button>
+        </header>
+        <div class="container"
+            @mousemove="move"
+            @mouseup="endMove"
+        >
+            <img :src="image" :style="zoomStyle" draggable=false
+                @mousedown="startMove"
+            >
+        </div>
+        <footer class="modal-footer">
+            <button class="zoom-in btn-zoom" @click="zoomIn">+</button>
+            <button class="zoom-reset btn-zoom" @click="zoomReset">=</button>
+            <button class="zoom-out btn-zoom" @click="zoomOut">-</button>
+        </footer>
     </div>
 </template>
 
@@ -18,93 +23,125 @@
 import Vue from 'vue';
 
 export default Vue.component('image-modal', {
-  template: '#image-modal',
-  props: [
-    'image'
-  ],
-  data: function() {
-    return {
-      scale: 1,
-    };
-  },
-  computed: {
-    zoomStyle: function() {
-      return {
-        transform: `scale(${this.scale}, ${this.scale})`
-      };
+    template: '#image-modal',
+    props: [
+        'image'
+    ],
+    data: function() {
+        return {
+            scale: 1,
+            imageX: 0,
+            imageY: 0,
+            //
+            dragStartPos: null,
+        };
+    },
+    computed: {
+        zoomStyle: function() {
+            return {
+                transform: `translate(${this.imageX}px, ${this.imageY}px)`
+                //transform: `scale(${this.scale}, ${this.scale})`
+            };
+        },
+        isMoving: function() {
+            return this.dragStartPos !== null;
+        }
+    },
+    methods: {
+        close: function() {
+            this.$emit('close');
+        },
+        // zoom
+        zoomIn: function() {
+            this.scale += 1;
+        },
+        zoomReset: function() {
+            this.scale = 1;
+        },
+        zoomOut: function() {
+            this.scale -= 1;
+        },
+        // move
+        startMove: function(e) {
+            this.isMoving = true;
+            this.dragStartPos = {x: e.x, y: e.y};
+        },
+        move: function(e) {
+            if (!this.isMoving) {
+                return;
+            }
+            var dragDistanceX = e.x - this.dragStartPos.x;
+            var dragDistanceY = e.y - this.dragStartPos.y;
+
+            this.imageX += dragDistanceX;
+            this.imageY += dragDistanceY;
+
+            this.dragStartPos.x = e.x;
+            this.dragStartPos.y = e.y;
+        },
+        endMove: function(e) {
+            if (!this.isMoving) {
+                return;
+            }
+            this.dragStartPos = null;
+        }
+    },
+    mounted: function() {
+        var self = this;
+        $('body').on('keydown.closeImageModal', function(e) {
+            if (e.keyCode === 27) {
+                self.close();
+            }
+        })
+    },
+    destroyed: function() {
+        $('body').off('keydown.closeImageModal');
     }
-  },
-  methods: {
-    close: function() {
-      this.$emit('close');
-    },
-    zoomIn: function() {
-      this.scale += 1;
-    },
-    zoomReset: function() {
-      this.scale = 1;
-    },
-    zoomOut: function() {
-      this.scale -= 1;
-    },
-  },
-  mounted: function() {
-    var self = this;
-    $('body').on('keydown.closeImageModal', function(e) {
-      if (e.keyCode === 27) {
-        self.close();
-      }
-    })
-  },
-  destroyed: function() {
-    $('body').off('keydown.closeImageModal');
-  }
 });
 </script>
 
-<style>
+<style scoped>
 .modal-main {
-  background-color: rgba(0, 0, 0, 0.8);
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 100;
+    background-color: rgba(0, 0, 0, 0.8);
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 100;
 }
 
-.modal-header {
-  background-color: black;
-  width: 100%;
-  height: 70px;
-  color: white;
+header {
+    background-color: black;
+    width: 100%;
+    height: 70px;
+    color: white;
 }
 
 .btn-close {
-  font-size: 24px;
-  width: 40px;
-  height: 40px;
+    font-size: 24px;
+    width: 40px;
+    height: 40px;
 }
 .container {
     height: calc(100% - 170px);
-    overflow: auto;
+    overflow: hidden;
 }
 
 footer {
-  background-color: black;
-  position: absolute;
-  bottom: 0;
+    background-color: black;
+    position: absolute;
+    bottom: 0;
 
-  width: 100%;
-  height: 100px;
+    width: 100%;
+    height: 100px;
 
-  padding: 0;
-  font-size: 24px;
+    padding: 0;
+    font-size: 24px;
 }
 
 .button-zoom {
-  width: 50px;
-  height: 50px;
+    width: 50px;
+    height: 50px;
 }
 </style>
-
